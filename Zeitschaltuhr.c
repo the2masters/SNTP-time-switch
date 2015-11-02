41315f74bd5ad5c19480bbde1fbc807d2397ca89
Stefan Hellermann <stefan@the2masters.de>
2015-11-02 22:00:08 +0100

    asd

--- Zeitschaltuhr.c
+++ Zeitschaltuhr.c
@@ -9,57 +9,6 @@
 
 #include "rules.h"
 
-
-
-
-
-
-
-/*
-// Calculate which relay should be switched on or off
-void calc(time_t time)
-{
-	struct tm tm;
-	localtime_r(&time, &tm);
-	tm.tm_sec = 0;				// ignore seconds
-
-	tm.tm_hour = 23, tm.tm_min = 60;
-	nextCalc = my_mktime(&tm);		// recalculate not later than next midnight
-
-	// Default: Every Relay switched off
-	bool relayState[RelayCount] = {0};
-
-	for(uint8_t i = 0; i < ARRAY_SIZE(Orders); i++)
-	{
-		// Skip this order, if it's written for another weekday
-		if((Orders[i].day & _BV(tm.tm_wday)) == 0)
-			continue;
-
-		// Calculate Start timestamp
-		tm.tm_hour = Orders[i].starthour, tm.tm_min = Orders[i].startmin;
-		time_t start = my_mktime(&tm);
-		if(time < start)		// if it's before start time of this order
-		{				// recalculate not later than switch on time
-			if(nextCalc > start) nextCalc = start;
-		} else {
-			// Calculate Stop timestamp
-			tm.tm_hour = Orders[i].stophour, tm.tm_min = Orders[i].stopmin;
-			time_t stop = my_mktime(&tm);
-			if(time < stop)		// if it's between start and stop of this order
-			{			// recalculate not later then switch off time
-				if(nextCalc > stop) nextCalc = stop;
-				// Set Flag to set Relay
-				relayState[Orders[i].relay] = true;
-			}
-		}
-	}
-	// Set each relay to it's calculated state
-	for(Relay_t i = 0; i < ARRAY_SIZE(relayState); i++)
-	{
-		relay_set(i, relayState[i]);
-	}
-}*/
-
 int main(void)
 {
 	timer1_init(F_CPU);
@@ -72,7 +21,7 @@ int main(void)
 
 	for (;;)
 	{
-		// From now on, if anything happens in interrupt context which requires a new run of the main loop, it has to sleep_disable();
+		// From now on, if anything happens in interrupt context which requires a new run of the main loop, it has to call sleep_disable();
 		sleep_enable();
 
 		processNetworkPackets();
--- rules.c
+++ rules.c
@@ -10,12 +10,9 @@
 
 typedef uint8_t ruleNum_t;
 
-#if true != 1
-	#error "(bool)true is expected to be 1"
-#endif
-
-#define boolToRuleValue(val) (val + 1)
-#define RuleValueToBool(val) (val - 1)
+#define RuleValueOffset 1
+#define boolToRuleValue(val) (val + RuleValueOffset)
+#define RuleValueToBool(val) (val - RuleValueOffset)
 
 typedef enum {
 	pvUnknown = 0,
@@ -29,7 +26,7 @@ typedef enum {
 	ptLogic,
 	ptRelay,
 	ptTimeSwitch,
-	ptTimeLimit,
+	ptMonoflop,
 	ptSunrise,
 	ptSunset,
 	ptSNTP = -1,	// negative = remote dependency
@@ -37,17 +34,27 @@ typedef enum {
 } ruleType_t;
 
 typedef enum {
-	LogicOff,
-	LogicOn,
-	LogicNOT,
-	LogicAND,
-	LogicOR,
-	LogicNAND,
-	LogicNOR,
+	LogicXOR = _BV(6),
+	LogicINV_E = _BV(5),
+	LogicINV_D = _BV(4),
+	LogicINV_C = _BV(3),
+	LogicINV_B = _BV(2),
+	LogicINV_A = _BV(1),
+	LogicINV_Q = _BV(0),
+
+	LogicRepeat = 0,
+	LogicNOT = LogicINV_Q,
+	LogicForceOn = LogicINV_E,
+	LogicForceOff = LogicForceOn ^ LogicINV_Q,
+	LogicOR = 0,
+	LogicNOR = LogicOR ^ LogicINV_Q,
+	LogicAND = LogicINV_A ^ LogicINV_B ^ LogicINV_C ^ LogicINV_D ^ LogicINV_E ^ LogicINV_Q,
+	LogicNAND = LogicAND ^ LogicINV_Q,
+	LogicXNOR = LogicXOR ^ LogicINV_Q,
 } LogicType_t;
 
 typedef struct {
-	ruleType_t		typ;
+	ruleType_t		type;
 	ruleNum_t		dependIndex; // In case of negativ typ this is the port on the remote machine
 	union {
 		IP_Address_t	IP;
@@ -58,7 +65,13 @@ typedef struct {
 			int8_t	wdays;
 		} time;
 		struct {
-			ruleNum_t second; // Zero means no argument
+			bool	minmaxtime;	// false: minimum time, true: maximum time
+			int8_t	hour;
+			int8_t	min;
+			int8_t	sec;
+		} monoflop;
+		struct {
+			ruleNum_t second;	// Zero means no argument
 			ruleNum_t third;
 			ruleNum_t fourth;
 			LogicType_t type;
@@ -77,20 +90,22 @@ typedef struct {
 #define timerOff UINT32_MAX
 
 static const __flash ruleData_t ruleData[] = {
-	[0] = {.typ = ptSNTP, .dependIndex = 123, .data.IP = CPU_TO_BE32(0xC0A8C801)},
-	[1] = {.typ = ptRemote, .dependIndex = 1, .data.IP = CPU_TO_BE32(0xC0A8C802)},
-	[2] = {.typ = ptTimeSwitch, .dependIndex = 0, .data.time = {.backward = false, .hour = 12, .min = 0, .wdays = 0b01111111}},
-	[3] = {.typ = ptRelay, .dependIndex = 3, .data.hw = {.port = &PORTC, .bitValue = _BV(5)}},
+	[0] = {.type = ptSNTP, .dependIndex = 123, .data.IP = CPU_TO_BE32(0xC0A8C801)},
+	[1] = {.type = ptRemote, .dependIndex = 1, .data.IP = CPU_TO_BE32(0xC0A8C802)},
+	[2] = {.type = ptTimeSwitch, .dependIndex = 0, .data.time = {.backward = false, .hour = 12, .min = 0, .wdays = 0b01111111}},
+	[3] = {.type = ptRelay, .dependIndex = 3, .data.hw = {.port = &PORTC, .bitValue = _BV(5)}},
 };
 
 static ruleState_t ruleState[ARRAY_SIZE(ruleData)] = {{0}};
 
 
-
-// This is mktime without changing it's arguments but recalculate dst each time
-// { struct tm tm = *time; tm.tm_isdst = -1; return mktime(&tm); }
-static time_t my_mktime(const struct tm *time)
+static time_t calculateTimestamp(struct tm *time, int8_t hour, int8_t min)
 {
+	time->tm_hour = hour, time->tm_min = min; // tm_sec already 0
+
+	// Following is mktime without changing it's arguments but recalculate dst each time
+	// time->tm_isdst = -1; return mktime(time);
+
 	// extract internals out of time.h implementation
 	extern int32_t __utc_offset;
 	extern int16_t (*__dst_ptr)(const time_t *, int32_t *);
@@ -106,12 +121,33 @@ static time_t my_mktime(const struct tm *time)
 	else
 		return retVal;
 }
-
-ATTR_PURE static bool checkDependency(ruleNum_t dependIndex, bool *value, bool *changed)
+static time_t _nextMidnight = 0;
+static struct tm *getStructTM(time_t now)
 {
+	static struct tm tm = {0};
+	if(now >= _nextMidnight)
+	{
+		localtime_r(&now, &tm);
+		tm.tm_sec = 0;	// ignore seconds
+
+		_nextMidnight = calculateTimestamp(&tm, 24, 0);
+	}
+	return &tm;
+}
+// Parameter to force usage only after getStructTM
+static time_t getMidnight(struct tm *tm ATTR_MAYBE_UNUSED)
+{
+	return _nextMidnight;
+}
+
+ATTR_PURE static bool checkDependency(ruleNum_t dependIndex, ruleNum_t noDependIndex, bool *value, bool *changed)
+{
+	if(dependIndex == noDependIndex)
+		return false;
+
 	ruleValue_t dependency = ruleState[dependIndex].value;
 	if(dependency == pvUnknown)
-		return false;
+		return true;
 
 	if(dependency < pvUnknown)
 	{
@@ -120,7 +156,7 @@ ATTR_PURE static bool checkDependency(ruleNum_t dependIndex, bool *value, bool *
 	}
 	*value = RuleValueToBool(dependency);
 
-	return true;
+	return false;
 }
 
 void checkRules(void)
@@ -129,93 +165,119 @@ void checkRules(void)
 
 	for(ruleNum_t rule = 0; rule < ARRAY_SIZE(ruleData); rule++)
 	{
-		bool dependValue = false;
+		bool ruleValue = false;
 		bool dependChanged = false;
 
-		// Do we depend on another rule?
-		if(ruleData[rule].typ >= 0 && ruleData[rule].dependIndex != rule)
-		{
-			if(!checkDependency(ruleData[rule].dependIndex, &dependValue, &dependChanged))
-				continue;
-		}
-
-		uint8_t ptlogic_dependCount = 0;
-		bool ptlogic_dependValues[3];
-		if(ruleData[rule].typ == ptLogic && ruleData[rule].data.logic.type > LogicNOT)
-		{
-			if(ruleData[rule].data.logic.second)
-			{
-				if(!checkDependency(ruleData[rule].data.logic.second, ptlogic_dependValues + ptlogic_dependCount, &dependChanged))
-					continue;
-				else
-					ptlogic_dependCount++;
-			}
-			if(ruleData[rule].data.logic.third)
-			{
-				if(!checkDependency(ruleData[rule].data.logic.third, ptlogic_dependValues + ptlogic_dependCount, &dependChanged))
-					continue;
-				else
-					ptlogic_dependCount++;
-			}
-			if(ruleData[rule].data.logic.fourth)
-			{
-				if(!checkDependency(ruleData[rule].data.logic.fourth, ptlogic_dependValues + ptlogic_dependCount, &dependChanged))
-					continue;
-				else
-					ptlogic_dependCount++;
-			}
-		}
-
-		// If we depend on an rule which didn't changed in this run and our timer has not expired, skip this rule
-		if(!dependChanged && ruleState[rule].timer < now)
+		// Check if dependency is in state unknown, otherwise set ruleValue to dependencyValue
+		if(ruleData[rule].type >= 0 && checkDependency(ruleData[rule].dependIndex, rule, &ruleValue, &dependChanged))
 			continue;
 
-		// Set default for result of this rule
-		ruleValue_t newValue = boolToRuleValue(dependValue);
+		// For ptLogic type we need to check more dependencies for unknown and changed values.
+		bool ptlogicValues[4] = {0};	// Save State of input B - E
+		if(ruleData[rule].type == ptLogic)
+		{
+			if(checkDependency(ruleData[rule].data.logic.second, 0, &ptlogicValues[0], &dependChanged))
+				continue;
+
+			if(checkDependency(ruleData[rule].data.logic.third, 0, &ptlogicValues[1], &dependChanged))
+				continue;
+
+			if(checkDependency(ruleData[rule].data.logic.fourth, 0, &ptlogicValues[2], &dependChanged))
+				continue;
+
+			// Fixed input E is always false
+		}
+
+
+		// If we depend on a rule which didn't change in this run and our timer has not expired, skip this rule
+		if(!dependChanged && now < ruleState[rule].timer)
+			continue;
+
+		// Set few defaults for following rules
+		bool resultUnknown = false;
 		time_t newTimer = timerOff;
 
-		switch(ruleData[rule].typ)
+		switch(ruleData[rule].type)
 		{
-			case ptLogic:
-				switch(ruleData[rule].data.logic.type)
-				{
-					case LogicOff:	dependValue = false; break;
-					case LogicOn:	dependValue = true; break;
-					case LogicNOT:	dependValue = !dependValue; break;
-					case LogicAND:	for(uint8_t i = ptlogic_dependCount; i; --i) 
-								dependValue &= ptlogic_dependValues[i];
-							break;
-					case LogicOR:	for(uint8_t i = ptlogic_dependCount; i; --i) 
-								dependValue |= ptlogic_dependValues[i];
-							break;
-					case LogicNAND:	for(uint8_t i = ptlogic_dependCount; i; --i) 
-								dependValue &= ptlogic_dependValues[i];
-							dependValue = !dependValue; 
-							break;
-					case LogicNOR:	for(uint8_t i = ptlogic_dependCount; i; --i) 
-								dependValue |= ptlogic_dependValues[i];
-							dependValue = !dependValue; 
-							break;
-				}
-				newValue = boolToRuleValue(dependValue);
+			case ptLogic: ;
+				LogicType_t logictype = ruleData[rule].data.logic.type;
+
+				if((logictype & LogicINV_A) == LogicINV_A)
+					ruleValue = !ruleValue;
+				if((logictype & LogicINV_B) == LogicINV_B)
+					ptlogicValues[0] = !ptlogicValues[0];
+				if((logictype & LogicINV_C) == LogicINV_C)
+					ptlogicValues[1] = !ptlogicValues[1];
+				if((logictype & LogicINV_D) == LogicINV_D)
+					ptlogicValues[2] = !ptlogicValues[2];
+				if((logictype & LogicINV_E) == LogicINV_E)
+					ptlogicValues[3] = !ptlogicValues[3];
+
+				if((logictype & LogicXOR) == LogicXOR)
+					ruleValue ^= ptlogicValues[0] ^ ptlogicValues[1] ^ ptlogicValues[2] ^ ptlogicValues[3];
+				else
+					ruleValue |= ptlogicValues[0] | ptlogicValues[1] | ptlogicValues[2] | ptlogicValues[3];
+
+				if((logictype & LogicINV_Q) == LogicINV_Q)
+					ruleValue = !ruleValue;
 				break;
+
 			case ptRelay:
-				if(dependValue)
+				if(ruleValue)
 					*ruleData[rule].data.hw.port |= ruleData[rule].data.hw.bitValue;
 				else
 					*ruleData[rule].data.hw.port &= ~ruleData[rule].data.hw.bitValue;
 				break;
 
+			case ptTimeSwitch: ;
+				struct tm *tm = getStructTM(now);
+
+				if((ruleData[rule].data.time.wdays & _BV(tm->tm_wday)) == 0)
+				{
+					ruleValue = false;
+					newTimer = getMidnight(tm);
+					break;
+				}
+
+				ruleValue = ruleData[rule].data.time.backward;
+				time_t switchtime = calculateTimestamp(tm, ruleData[rule].data.time.hour, ruleData[rule].data.time.min);
+
+				if(now < switchtime)
+				{
+					newTimer = switchtime;
+				} else {
+					newTimer = getMidnight(tm);
+					ruleValue = !ruleValue;
+				}
+				break;
+
+			case ptMonoflop:
+				if(ruleState[rule].timer == timerOff)
+				{
+					if(dependChanged && ruleValue)
+					{
+						newTimer = now + (ruleData[rule].data.monoflop.hour * (int16_t)60 + ruleData[rule].data.monoflop.min) * (int32_t)60 + ruleData[rule].data.monoflop.sec;
+					}
+				}
+				else if(now < ruleState[rule].timer)
+				{
+					ruleValue = true;
+				} else {
+					ruleValue = false;
+					newTimer = timerOff;
+				}
+
 			case ptSNTP:
 			case ptRemote:
-				newValue = pvUnknown;
+				resultUnknown = true;
+
 				Packet_t sendPacket;
 				// Check if USB Queue is free
 				if(USB_isReady() && USB_prepareTS(&sendPacket))
 				{
 					int8_t length;
 
-					if(ruleData[rule].typ == ptSNTP)
+					if(ruleData[rule].type == ptSNTP)
 						length = SNTP_GenerateRequest(sendPacket.data, &ruleData[rule].data.IP, ruleData[rule].dependIndex);
 					else // typ = ptRemote
 						length = ASCII_GenerateRequest(sendPacket.data, &ruleData[rule].data.IP, ruleData[rule].dependIndex);
@@ -234,11 +296,15 @@ void checkRules(void)
 				}
 				break;
 		}
+		ruleState[rule].timer = newTimer;
+		if(resultUnknown)
+			continue;
+
+		ruleValue_t newValue = boolToRuleValue(ruleValue);
 
 		// Did the result of our rule change? Then set changeflag (inverted value)
-		if(newValue != ruleState[rule].value)
+		if(ruleState[rule].value != newValue)
 			ruleState[rule].value = -newValue;
-		ruleState[rule].timer = newTimer;
 	}
 }
 
@@ -288,13 +354,13 @@ void UDP_Callback_Reply(uint8_t packet[], const IP_Address_t *sourceIP, UDP_Port
 {
 	for(ruleNum_t rule = 0; rule < ARRAY_SIZE(ruleData); rule++)
 	{
-		if(ruleData[rule].typ >= 0) continue;
+		if(ruleData[rule].type >= 0) continue;
 		if(ruleData[rule].dependIndex != sourcePort) continue;
 		if(ruleData[rule].data.IP != *sourceIP) continue;
 
 		// Now we know the packet is for the current rule
 		ruleValue_t newState;
-		if(ruleData[rule].typ == ptSNTP)
+		if(ruleData[rule].type == ptSNTP)
 		{
 			time_t newTime = SNTP_ProcessPacket(packet, length);
 			if(!newTime) break;
